af5c59663a6ef114994f19c38cb072dd92aca3e3
renaaltub <renata.ltube@gmail.com>
2025-10-28 11:43:56 -0300

    .

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from funciones2 import *
 from playlist import *
 from normalizacion import * 
 
-normalizar_datos_completos(playlist_lady_gaga)
+playlist_normalizada = normalizar_datos_completos(playlist_lady_gaga)
 
 while True:
     opcion = input("1. Mostar lista de temas\n2. Temas ordenados por duracion\n3. Promedio de vistas\n4. Video/s mas visto/s\n5. Video/s menos visto/s\n6. Buscar video por codigo\n7. Listar colaborador\n8. Listar por mes de lanzamiento\n9. Salir\nElija una: ")
